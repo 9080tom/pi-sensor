be6d119a4799e35f89f40724f5f246a1686f21f3
9080tom <t.thickett@hotmail.co.uk>
2019-05-30 17:51:36 +0100

    push notification test

---
+++ push.py
@@ -0,0 +1,2 @@
+import requests
+requests.post(url = "https://exp.host/--/api/v2/push/send", data = {"to": "ExponentPushToken[92Fj8oKMlp66XPkYSwCFGi]", "title": "Movement Detected", "body": "New video has been uploaded"})
