3e7254be69666005d436a579b425f34df2343fc6
9080tom <t.thickett@hotmail.co.uk>
2019-06-06 12:27:39 +0100

    deleated some unnecicary files and modifyed settings

--- push.py
+++
@@ -1,2 +0,0 @@
-import requests
-requests.post(url = "https://exp.host/--/api/v2/push/send", data = {"to": "ExponentPushToken[92Fj8oKMlp66XPkYSwCFGi]", "title": "Movement Detected", "body": "New video has been uploaded"})
